e9f0fecc639a6553d63de8cf8c1d9657e6c9c408
unknown <you@example.com>
2018-04-11 22:24:26 +0800

    complete and test the main class-Library, which controls the logic in the whole project.

--- src/main/java/com/tw/Library.java
+++ src/main/java/com/tw/Library.java
@@ -1,7 +1,99 @@
 package com.tw;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Library {
-    public boolean someLibraryMethod() {
-        return true;
+    private Manage manager = new Manage();
+
+    public void welcomeToTheLibrary() {
+        while (true) {
+            displayMenu();
+            Scanner sc = new Scanner(System.in);//将系统输入复制给变量
+            String num = sc.nextLine();
+            // 如果选择功能1
+            if (num.equals("1")) {
+                System.out.print("请输入学生信息（格式：姓名, 学号, 学科: 成绩, ...），按回车提交：\n");
+                String[] info = sc.nextLine().split(", ");
+                displayPageOfFunction1(info);
+            }
+            // 如果选择功能2
+            else if (num.equals("2")) {
+                System.out.print("请输入要打印的学生的学号（格式： 学号, 学号,...），按回车提交：\n");
+                String[] info = sc.nextLine().split(", ");
+                displayPageOfFunction2(info);
+            }
+            // 如果选择功能3
+            else if (num.equals("3")) {
+                System.exit(0);
+            }
+        }
+    }
+
+    // 显示主菜单
+    public void displayMenu() {
+        System.out.print("1. 添加学生\n2. 生成成绩单\n3. 退出\n请输入你的选择（1～3）：\n");
+    }
+
+    // 显示功能1页面
+    public void displayPageOfFunction1(String[] info) {
+        // 判断输入格式是否是正确的Student格式
+        if (isCorrectFormatOfStudent(info)) {
+            ArrayList<Item> itemlist = new ArrayList<>();
+            for (int i = 2; i < info.length; i++) {
+                String[] item_info = info[i].split(": ");
+                Item item = new Item(item_info[0], Integer.parseInt(item_info[1]));
+                itemlist.add(item);
+            }
+            Student student = new Student(info[0], info[1], itemlist);
+            manager.addStudent(student);
+            System.out.print("学生" + student.getName() + "的成绩被添加\n");
+        } else {
+            System.out.print("请按正确的格式输入（格式：姓名, 学号, 学科: 成绩, ...）：\n");
+        }
+    }
+
+    // 判断输入格式是否是正确的Student格式
+    public boolean isCorrectFormatOfStudent(String[] info) {
+        boolean flag = true;
+        if (info == null || info.length != 6) {
+            flag = false;
+        } else {
+            for (int i = 2; i < info.length; i++) {
+                String[] item_info = info[i].split(": ");
+                if (item_info == null || item_info.length != 2) {
+                    flag = false;
+                }
+            }
+        }
+        return flag;
+    }
+
+    // 显示功能2页面
+    public void displayPageOfFunction2(String[] info) {
+        // 判断输入格式是否是正确的学号列表格式
+        if (isCorrectFormatOfIdList(info)) {
+            manager.printGradeList(info);
+        } else {
+            System.out.print("请按正确的格式输入要打印的学生的学号（格式： 学号, 学号,...），按回车提交：\n");
+        }
+    }
+
+    // 判断输入格式是否是正确的学号列表格式，假设学号长度必须为3
+    public boolean isCorrectFormatOfIdList(String[] info) {
+        boolean flag = true;
+        if (info == null || info.length >= 1) {
+            for (int i = 0; i < info.length; i++) {
+                if (info[i].length() != 3) {
+                    flag = false;
+                }
+            }
+        }
+        return flag;
+    }
+
+    public static void main(String[] args) {
+        Library library = new Library();
+        library.welcomeToTheLibrary();
     }
 }
--- src/test/java/com/tw/LibraryTest.java
+++ src/test/java/com/tw/LibraryTest.java
@@ -1,32 +1,69 @@
 package com.tw;
 
+import org.junit.Before;
 import org.junit.Test;
-
-import java.util.LinkedList;
-
-import static org.hamcrest.core.Is.is;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import static org.junit.Assert.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 public class LibraryTest {
-    @Test
-    public void testSomeLibraryMethod() {
-        Library classUnderTest = new Library();
-        assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+    private Library library;
+    private ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUp() throws Exception {
+        library = new Library();
+        System.setOut(new PrintStream(outContent));
+    }
+
+    private String systemOut() {
+        return outContent.toString();
     }
 
     @Test
-    public void testMockClass() throws Exception {
-        // you can mock concrete classes, not only interfaces
-        LinkedList mockedList = mock(LinkedList.class);
-
-        // stubbing appears before the actual execution
-        String value = "first";
-        when(mockedList.get(0)).thenReturn(value);
-
-        assertEquals(mockedList.get(0), value);
-
+    public void test_displayMenu() {
+        library.displayMenu();
+        assertEquals(systemOut(), "1. 添加学生\n2. 生成成绩单\n3. 退出\n请输入你的选择（1～3）：\n");
     }
 
+    @Test
+    public void test_displayPageOfFunction1_addSuccessful() {
+        String str = "张三, 001, 数学: 75, 语文: 95, 英语: 80, 编程: 80";
+        String[] info = str.split(", ");
+        library.displayPageOfFunction1(info);
+        assertEquals(systemOut(), "学生张三的成绩被添加\n");
+    }
+
+    @Test
+    public void test_displayPageOfFunction1_addFailed() {
+        String str = "张三, 001, 数学: 75, 语文: 95";
+        String[] info = str.split(", ");
+        library.displayPageOfFunction1(info);
+        assertEquals(systemOut(), "请按正确的格式输入（格式：姓名, 学号, 学科: 成绩, ...）：\n");
+    }
+
+    @Test
+    public void test_isCorrectFormatOfStudent() {
+        String[] info = new String[]{"张三", "001", "数学: 75", "语文: 95", "英语: 80", "编程: 80"};
+        assertEquals(library.isCorrectFormatOfStudent(info), true);
+        String[] info2 = new String[]{"张三", "001", "数学: 75", "语文: 95"};
+        assertEquals(library.isCorrectFormatOfStudent(info2), false);
+    }
+
+    @Test
+    public void test_displayPageOfFunction2_addFailed() {
+        String str = "001、 002、";
+        String[] info = str.split(", ");
+        library.displayPageOfFunction2(info);
+        assertEquals(systemOut(), "请按正确的格式输入要打印的学生的学号（格式： 学号, 学号,...），按回车提交：\n");
+    }
+
+    @Test
+    public void test_isCorrectFormatOfIdList() {
+        String[] info = new String[]{"001", "002"};
+        assertEquals(library.isCorrectFormatOfIdList(info), true);
+        String[] info2 = new String[]{"001002"};
+        assertEquals(library.isCorrectFormatOfIdList(info2), false);
+    }
 }
+
